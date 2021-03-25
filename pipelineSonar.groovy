84bc3f557032c0c3e8edf42a833b5136f2e3ec7e
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2021-03-25 15:43:31 -0300

    update

--- pipelineSonar.groovy
+++ pipelineSonar.groovy
@@ -6,11 +6,10 @@ node {
     def scannerHome = tool 'sonarqube';
     withSonarQubeEnv('sonarqube') {
       sh "${scannerHome}/bin/sonar-scanner \
-      -D sonar.login=admin \
-      -D sonar.password=123456 \
-      -D sonar.projectKey=Sonarqube \
-      -D sonar.exclusions=vendor/**,resources/**,**/*.java \
-      -D sonar.host.url=http://localhost:9000/"
+      -Dsonar.organization=marcosvinirocha \
+      -Dsonar.projectKey=testes-algar \
+      -Dsonar.sources=. \
+      -Dsonar.host.url=https://sonarcloud.io
     }
   }
 }
